e6f1cfae041700640d4c91d4425ee7e23a3d9c22
Chinh Nguyen <monkeynhucci@gmail.com>
2025-05-06 11:19:32 +0700

    Snapshot DB

--- ent/migrate/migrations/20250506041856_init_snapshot.sql
+++ ent/migrate/migrations/20250506041856_init_snapshot.sql
@@ -2,3 +2,5 @@
 CREATE TABLE `cars` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `model` text NOT NULL, `registered_at` datetime NOT NULL, `user_cars` integer NULL, CONSTRAINT `cars_users_cars` FOREIGN KEY (`user_cars`) REFERENCES `users` (`id`) ON UPDATE NO ACTION ON DELETE SET NULL);
 -- Create "groups" table
 CREATE TABLE `groups` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `name` text NOT NULL);
+-- Create "users" table
+CREATE TABLE `users` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `age` integer NOT NULL, `name` text NOT NULL DEFAULT 'unknown');
